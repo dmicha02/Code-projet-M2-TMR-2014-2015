74168c7cb72a69adaa954e5e51e9eaf0d6b4ffeb
dmicha02 <mich260@gmail.com>
2015-01-29 10:37:58 +0100

    ajout de l'algorithme de recalage d'images
    ajout dans le programme principale l'algorithme de Matthews Baker
    compositionnel inverse + déformation des images. Il manque la partie de
    construction du panorama

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include "Matching/ECC.h"
 #include "loadtextfile.h"
 #include "data2img.h"
+#include "stitching.h"
 
 using namespace std;
 using namespace cv;
@@ -24,14 +25,14 @@ using namespace cv;
  */
 static void help()
 {
-	cout << "Endomicroscopic images reconstruction program\n"\
-				"\n"<<endl;
+    cout << "\nRebuildind endomicroscopic images.\n"\
+		"M2 TMR Project 2014-2015\n"\
+		"**********************************\n\n"<< endl;
 }
 
 /**
  * \fn int main (int argc, char** argv)
- * \brief Main program.
- *
+ * \brief Main program of M2 project 2014-2015 : Endomicroscopic images reconstruction
  * \return EXIT_SUCCESS - Normal stop of program.
  */
 int main(int argc, char** argv) {
@@ -40,102 +41,79 @@ int main(int argc, char** argv) {
 //****************************************************************************************************************
 //								Images reconstruction with analogic datas
 //****************************************************************************************************************/
-	Mat data, img, img_reconstr;
-	
-	//Load text file in a cv::Mat
-	data = load_textfile2Mat("Papier.txt"); //chargement du fichier contenant les données analogiques
-	
-	timer__("Time of images reconstruction")
-	{
-		//cv::Mat of file to image form (cv::Mat 250*250)
-		timer__("File matrix to an image form")
-			img = data2img(data);
-		
-		////Mean Interpolation
-		//timer__("Mean Interpolation of image")
-		//	img_reconstr = interpolationMean(img);
+//	Mat data, img, img_reconstr;
+//	
+//	//Load text file in a cv::Mat
+//	data = load_textfile2Mat("Papier.txt"); //chargement du fichier contenant les données analogiques
+//	//cv::Mat of file to image form (cv::Mat 250*250)
+//	timer__("File matrix to an image form")
+//		img = data2img(data);
+//	//imshow("Originale image", img);	
+//	//Mean Interpolation
+//	timer__("Mean Interpolation of image")
+//		img_reconstr = interpolationMean(img);
+//	//imshow("Mean Interpolate", img_reconstr);
+//	//Median Interpolation
+//	timer__("Median Interpolation of image")
+//		img_reconstr = interpolationMedian(img);
+//	//imshow("Median Interpolate", img_reconstr);
+//	//Bilinear interpolation
+//	timer__("Bilinear Interpolation of image")
+//		img_reconstr = interpolationBilinear(img);
+//	//imshow("Bilinear Interpolate", img_reconstr);
 
-		////Median Interpolation
-		//timer__("Median Interpolation of image")
-		//	img_reconstr = interpolationMedian(img);
-
-		//Bilinear interpolation
-		timer__("Bilinear Interpolation of image")
-			img_reconstr = interpolationBilinear(img);
-	}
-	imshow("Originale image", img);
-	//imshow("Mean Interpolate", img_reconstr);
-	//imshow("Median Interpolate", img_reconstr);
-	imshow("Bilinear Interpolate", img_reconstr);
-
-/****************************************************************************************************************
-											Stiching images
-****************************************************************************************************************/
- //   string path;
- //   if (argc < 2) {
- //       path = "../img/Endoscope non lineaire_champ 250um/";
- //   } else {
- //       path = string(argv[1]);
- //   }
- //   
- //
- //   VideoCapture cap(path + "%03d.jpg");
- //   cap.set(CAP_PROP_FPS, 8);
- //   namedWindow("Camera", WINDOW_AUTOSIZE);
- //   namedWindow("Panorama", WINDOW_AUTOSIZE);
-
- //   Mat img;
-
- //   //Process on first image
- //   while (!cap.read(img));
- //   cvtColor(img, img, COLOR_BGR2GRAY);
-	//img = interpolationMean(img); // reconstruction of image
- //   
-	//// init for template matching
- //   Mat img_fusion = Mat::zeros(768 * 2, 1024 * 2, CV_8U);
- //   Rect img_center_fusion;
- //   img_center_fusion.x = img_fusion.cols / 2 - img.cols / 2;
- //   img_center_fusion.y = img_fusion.rows / 2 - img.rows / 2;
- //   img_center_fusion.width = img.cols;
- //   img_center_fusion.height = img.rows;
- //   img.copyTo(img_fusion(img_center_fusion));
- //   TemplateMatching m(img_center_fusion, img_fusion, SEQUENCE);
-
-	////Process on other images
- //   while (true) {
-
- //       if (cap.read(img)) {
- //           cvtColor(img, img, COLOR_BGR2GRAY);
-	//		//imshow("Original image", img);
- //           timer__("Correction") {
- //               img = interpolationMean(img);
- //           }
-
- //           bool matched;
- //           timer__("Matching")
-	//			matched = m.match(img);
-
- //           if (likely(matched))
- //               timer__("Fusion")
-	//				m.blend(img_fusion);
-
- //           Mat img_fusion_mini;
- //           resize(img_fusion, img_fusion_mini, Size(), 0.5, 0.5, INTER_LINEAR);
-
- //           imshow("Panorama", img_fusion_mini);
- //           imshow("Camera", img);
- //           waitKey();
- //       }
- //       if (waitKey(30) >= 0) break;
- //   }
-
- //   if (argc < 3) {
- //       m.build_final("out.png");
- //   } else {
- //       m.build_final(string(argv[2]));
- //   }
-
-	waitKey(0);    
+//****************************************************************************************************************
+//											Stiching images
+//****************************************************************************************************************/
+    string path;
+    if (argc < 2) {
+        path = "../img/Endoscope non lineaire_champ 250um/";
+    } else {
+        path = string(argv[1]);
+    }
+	/// Initialisation
+	Mat img0, target_img, img, img2, template_img, W_estimate;
+    /// Capture of images
+    VideoCapture cap(path + "%03d.jpg");
+    cap.set(CAP_PROP_FPS, 8);
+    namedWindow("Camera", WINDOW_AUTOSIZE);
+	namedWindow("Rebuilded", WINDOW_AUTOSIZE);
+	//namedWindow("Panorama", WINDOW_AUTOSIZE);
+    /// Process on first image
+	while (!cap.read(img));
+	img.convertTo(img, CV_32F); // Convert in float32
+    cvtColor(img, img, COLOR_RGB2GRAY); // RGB -> Grayscale
+	img = img / 255.0; // Value between 0 and 1
+	//imshow("Camera", img/255);
+	img0 = interpolationMean(img); // reconstruction of image
+    //imshow("Rebuilded",img0);
+	/// Init template
+	Rect rec_tmp(Point(img0.rows/6,img0.cols/6), Point(5*img0.rows/6,5*img0.cols/6)); // Template rectangle
+	template_img = img0(Range(img0.rows/6,5*img0.rows/6),Range(img0.cols/6,5*img0.cols/6)); // Template image
+	///Process on other images
+    while (true) {
+        if (cap.read(img)) {
+            img.convertTo(img, CV_32F); // Convert in float32
+			cvtColor(img, img, COLOR_RGB2GRAY); // RGB -> Grayscale
+			target_img = img / 255.0; // Value between 0 and 1
+			imshow("Camera", img/255);
+			/// Image reconstruction
+            timer__("Correction")
+                target_img = interpolationMean(target_img); // interpolation with mean value in neighborhood
+			/// Matthews-Baker inverse compositionnal algorithm
+			timer__("Template Matching")
+				W_estimate = align_image(target_img, template_img, rec_tmp); // inverse compositional algorithm
+			cout << W_estimate << endl;
+			warpPerspective(target_img, img2, W_estimate.inv(), target_img.size()); // warp images
+			img2.copyTo(img0); // Previous image
+			Rect rec_tmp(Point(img0.rows/6,img0.cols/6), Point(5*img0.rows/6,5*img0.cols/6)); // Template rectangle
+			template_img = img0(Range(img0.rows/6,5*img0.rows/6),Range(img0.cols/6,5*img0.cols/6)); // Template image
+            imshow("Rebuilded", target_img);
+            //waitKey();
+        }
+        if (waitKey(30) >= 0) break;
+    }
+	waitKey(0);   
     return EXIT_SUCCESS;
 }
 
