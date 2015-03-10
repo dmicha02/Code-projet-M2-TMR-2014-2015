177632f46e6ec78a495d5e472ead2db459ebf95b
dmicha02 <mich260@gmail.com>
2015-03-10 10:00:59 +0100

    Revert "ajout des codes"
    This reverts commit 34759466abb00d981cb45dbd95e2e41f7d0fb2c2.

--- Codes_GPHY/Olga_Yousra/invc.h
+++
@@ -1,8 +0,0 @@
-#ifndef INVC_H
-#define INVC_H
-
-#include "cv.h"
-
-void align_image_inverse_compositional(cv::Mat pImgT, cv::Rect omega, cv::Mat pImgI);
-
-#endif //__INVC_H
--- Codes_GPHY/correction.h
+++
@@ -1,9 +0,0 @@
-#ifndef CORRECTION_H
-#define	CORRECTION_H
-#include <opencv2/opencv.hpp>
-
-void Interpolation(cv::Mat img, cv::Mat& dst);
-void swirl_init(cv::Size img_size, cv::Point center, float radius, float rotation, float strength, cv::Mat& swirl_data);
-void swirl(cv::Mat img, cv::Mat& dst, cv::Mat& swirl_data);
-#endif	/* CORRECTION_H */
-
